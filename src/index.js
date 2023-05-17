4259b6165d06914439d8ca107a23260d6609856c
JunyiLye <junyilye@gmail.com>
2023-05-17 14:57:03 +0800

    w14

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App_34 from './App_34';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App_34 />
+  </React.StrictMode>
+);
+
+
