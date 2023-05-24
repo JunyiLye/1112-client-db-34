01ad85905afed6df03b5291db64b84ff72e66d81
JunyiLye <junyilye@gmail.com>
2023-05-24 14:38:39 +0800

    w14yes

--- src/App_34.js
+++ src/App_34.js
@@ -5,6 +5,16 @@ function App() {
   
   const getShop2_34 = async () => {
       try{
+        const response = await fetch(`https://fvrblhdevzlksdpibgfv.supabase.co/rest/v1/shop2_34?select=*`, {
+          method: 'GET',
+          headers: {
+            apikey: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2cmJsaGRldnpsa3NkcGliZ2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0ODg2NDIsImV4cCI6MTk5ODA2NDY0Mn0.FgF-29UktvIYFlC5BAfr3q83RZ0Noa9rseBX2SrmCeQ`,
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2cmJsaGRldnpsa3NkcGliZ2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0ODg2NDIsImV4cCI6MTk5ODA2NDY0Mn0.FgF-29UktvIYFlC5BAfr3q83RZ0Noa9rseBX2SrmCeQ`,
+          }
+        });
+        const data = await response.json();
+        console.log('shop2 data', data);
+        setProducts(data);
       }catch(error){
         console.log(error);
       }
@@ -17,16 +27,22 @@ function App() {
   return (
   <div className="shop-page">
     <div className="collection-page">
-      <h1 className="title">Womens</h1>
+      <h1 className="title">All Products -- Supabase shop2</h1>
       <div className="items">
-        <div className="collection-item">
-          <img className="image" src="https://i.ibb.co/7CQVJNm/blue-tank.png" />
-          <div className="collection-footer">
-            <span className="name">Blue Tanktop</span>
-            <span className="price">25</span>
+        { products.map( (product) => {
+          const {name, price, local_url} = product;
+          return(
+            <div className="collection-item">
+            <img className="image" src={local_url} />
+            <div className="collection-footer">
+              <span className="name">{name}</span>
+              <span className="price">{price}</span>
+            </div>
+            <button className="custom-button">Add to Cart</button>
           </div>
-          <button className="custom-button">Add to Cart</button>
-        </div>
+          )
+        })}
+        
         <div className="collection-item">
           <img
             className="image"
@@ -38,58 +54,7 @@ function App() {
           </div>
           <button className="custom-button">Add to Card</button>
         </div>
-        <div className="collection-item">
-          <img
-            className="image"
-            src="https://i.ibb.co/KV18Ysr/floral-skirt.png"
-          />
-          <div className="collection-footer">
-            <span className="name">Floral Dress</span>
-            <span className="price">80</span>
-          </div>
-          <button className="custom-button">Add to Cart</button>
-        </div>
-        <div className="collection-item">
-          <img
-            className="image"
-            src="https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png"
-          />
-          <div className="collection-footer">
-            <span className="name">Red Dots Dress</span>
-            <span className="price">80</span>
-          </div>
-          <button className="custom-button">Add to Cart</button>
-        </div>
-        <div className="collection-item">
-          <img
-            className="image"
-            src="https://i.ibb.co/KmSkMbH/striped-sweater.png"
-          />
-          <div className="collection-footer">
-            <span className="name">Striped Sweater</span>
-            <span className="price">45</span>
-          </div>
-          <button className="custom-button">Add to Cart</button>
-        </div>
-        <div className="collection-item">
-          <img
-            className="image"
-            src="https://i.ibb.co/v1cvwNf/yellow-track-suit.png"
-          />
-          <div className="collection-footer">
-            <span className="name">Yellow Track Suite</span>
-            <span className="price">135</span>
-          </div>
-          <button className="custom-button">Add to Cart</button>
-        </div>
-        <div className="collection-item">
-          <img className="image" src="https://i.ibb.co/qBcrsJg/white-vest.png" />
-          <div className="collection-footer">
-            <span className="name">White Blouse</span>
-            <span className="price">20</span>
-          </div>
-          <button className="custom-button">Add to Cart</button>
-        </div>
+        
       </div>
     </div>
   </div>
